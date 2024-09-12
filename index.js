f385913dec27b1d939577ca907da72ad6b34759d
tomeytran <tmtri310251@gmail.com>
2024-09-12 10:12:58 +0700

    khai bao

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ let f = 20.5;
 let b = true;
 let s = "Hà Nội";
 document.write(
-	`i = ${i}    
-  f = ${f}  
-  b = ${b}  
-  s = ${s} `
+	`i = ${i}
+  <br />    
+  f = ${f} 
+  <br /> 
+  b = ${b}
+  <br />  
+  s = ${s} 
+  <br />`
 );
 // Bài 2
 let width = 20;
 let height = 10;
 let area = width * height;
-document.write("Area = " + area);
+document.write(`Area = ${area} <br />`);
 // Bài 3
-// let value_a = prompt("Nhap gia tri a");
-// value_a;
-// document.write(valueOf(value_a));
+showNumbers();
+function showNumbers() {
+	let value_a = prompt("Input numer a");
+	let value_b = prompt("Input number b");
+	alert(`Number a = ${value_a} and number b = ${value_b} `);
+	document.write(`Number a = ${value_a} and number b = ${value_b} `);
+}
