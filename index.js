f5d47d21fc0c6a2aa84664fcfafa47455ffc6e2d
tomeytran <tmtri310251@gmail.com>
2024-09-12 10:03:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Bài 1
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Hà Nội";
+document.write(
+	`i = ${i}    
+  f = ${f}  
+  b = ${b}  
+  s = ${s} `
+);
+// Bài 2
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write("Area = " + area);
+// Bài 3
+// let value_a = prompt("Nhap gia tri a");
+// value_a;
+// document.write(valueOf(value_a));
